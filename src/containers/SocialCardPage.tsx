8d717386b74caae45dcc81707a24fd9ce09ff501
JackLiR8 <jiakeli1024@gmail.com>
2020-01-09 10:32:18 +0800

    :sparkles: 引入iconfont

---
+++ src/containers/SocialCardPage.tsx
@@ -0,0 +1,15 @@
+/**
+ * @file build a social card
+ */
+
+import React from 'react'
+
+function SocialCard() {
+  return (
+    <div className="social-card">
+
+    </div>
+  )
+}
+
+export default SocialCard;
