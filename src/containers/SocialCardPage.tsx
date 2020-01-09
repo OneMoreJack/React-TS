d37f50edaf85ab57e303cc2606195ed1ca914332
JackLiR8 <jiakeli1024@gmail.com>
2020-01-09 15:55:18 +0800

    :construction: social-card 进行中

--- src/containers/SocialCardPage.tsx
+++ src/containers/SocialCardPage.tsx
@@ -5,10 +5,12 @@ import './SocialCardPage.scss'
 import React, { useState, useEffect } from 'react'
 
 interface cardInfo {
-  avator: string,
+  logo: string,
   title: string,
   author: string,
   date: string,
+  subTitle?: string,
+  avator?: string
 }
 
 interface headerProps {
@@ -16,10 +18,10 @@ interface headerProps {
 }
 
 function CardHeader(props: headerProps) {
-  const { avator, title, author, date} = props.info;
+  const { logo, title, author, date} = props.info;
   return (
     <header className="social-card-header">
-      <img className="logo" src={avator} alt="用户头像" />
+      <img className="logo" src={logo} alt="用户头像" />
       <div className="right">
         <div className="title">{ title }</div>
         <div>author: <span className="author">{ author }</span></div>
@@ -29,23 +31,53 @@ function CardHeader(props: headerProps) {
   )
 }
 
+interface bodyProps extends headerProps {
+
+}
+function CardBody(props: bodyProps) {
+  const { avator, title, author, subTitle} = props.info;
+  return (
+    <div className="social-card-body">
+      <div className="card-title-box">
+        <h2 className="title-text">{ title }</h2>
+      </div>
+      <div className="card-content">
+        <div className="title">{ title}</div>
+        <div>{ subTitle}</div>
+      </div>
+    </div>
+  )
+}
+
+function CardFooter() {
+  return (
+    <div className="social-card-footer">
+      footer
+    </div>
+  )
+}
+
 interface cardProps extends headerProps {
-  // info: cardInfo
+
 }
 function SocialCard(props: cardProps) {
   return (
     <div className="social-card">
       <CardHeader info={props.info} />
+      <CardBody info={props.info} />
+      <CardFooter />
     </div>
   )
 }
 
 const pageData = [
   {
-    avator: 'https://tse4-mm.cn.bing.net/th/id/OIP.IVzb3bA0416WB6Xf4GTgUgAAAA?w=173&h=170&c=7&o=5&pid=1.7',
+    logo: 'https://tse4-mm.cn.bing.net/th/id/OIP.IVzb3bA0416WB6Xf4GTgUgAAAA?w=173&h=170&c=7&o=5&pid=1.7',
     title: 'Learning React? Start Small.',
     author: 'Jack',
-    date: 'Jan 9'
+    date: 'Jan 9',
+    subTitle: 'This is my first react project with typescript. I can not wait to explore what typescript can give me.',
+    avatar: 'https://yc-cloud-1257265770.cos.ap-guangzhou.myqcloud.com/transfer/headPortrait/20190416183143_301088.jpg'
   }
 ]
 
@@ -57,13 +89,13 @@ function SocialPage() {
   }, []);
 
   return (
-    <>
+    <div style={{'minWidth': '800px'}}>
       {data && data.map((v, index) => {
         return (
           <SocialCard key={index} info={v} />
         )
       })}
-    </>
+    </div>
   )
 }
 
