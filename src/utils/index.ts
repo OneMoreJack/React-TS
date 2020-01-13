0b66f8b9fc04705fc121b43ed82f1ace8984b7ce
JackLiR8 <jiakeli1024@gmail.com>
2020-01-13 17:45:38 +0800

    :bug: 华氏温度转化错误

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ export function tempTransfer(temp: number, metric: string = 'celsius') {
   if (metric === 'celsius') {
     res = Number((temp - 273.15).toFixed())
   } else {
-    res = Number(temp.toFixed())
+    res = Number((1.8 * (temp - 273.15) + 32).toFixed())
   }
   return res;
 }
