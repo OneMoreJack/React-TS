3800ad306fae2f8058dcac564129115b41474216
JackLiR8 <jiakeli1024@gmail.com>
2020-02-06 00:26:05 +0800

    :construcion: 切歌进行中

--- src/containers/Music/PlayerBar/index.tsx
+++ src/containers/Music/PlayerBar/index.tsx
@@ -6,9 +6,10 @@ interface barParams {
   song: SongInfo | null
 }
 export default function PlayerBar(props: barParams) {
+
   return (
     <div className="player-bar">
-      <audio controls>
+      <audio className="music-player" autoPlay controls>
         {props.song && <source src={`https://v1.itooi.cn/netease/url?id=${props.song.id}`}></source>}
       </audio>
     </div>
